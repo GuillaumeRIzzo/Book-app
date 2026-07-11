5dfe8a340a020f260ce4f68e72c469a643b5750e
GuillaumeRizzo <guillaume.rizzo.lomet@gmail.com>
2026-07-11 14:55:16 +0200

    chore(front): update React dependencies

--- Front/ReactJS/next.config.mjs
+++ Front/ReactJS/next.config.mjs
@@ -1,59 +1,49 @@
-import withPlugins from 'next-compose-plugins';
-import withTM from 'next-transpile-modules';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false, // Désactivé car Babel est actif
   compiler: {
     styledComponents: true,
   },
-  reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {}, // <- vide mais valide
+    },
+  },
+  images: {
+    domains: [
+      'www.babelio.com', 
+      'images-na.ssl-images-amazon.com',
+      'm.media-amazon.com',
+      'upload.wikimedia.org',
+      'images.epagine.fr'], // ✅ Ici tu ajoutes tous les domaines autorisés
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false;
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        module: false,
+        v8: false,
+        perf_hooks: false,
+      };
     }
+    config.infrastructureLogging = { level: 'warn' };
     return config;
   },
   async rewrites() {
     return [
-      {
-        source: '/signin',
-        destination: '/auth/signin',
-      },
-      {
-        source: '/login',
-        destination: '/auth/login',
-      },
-      {
-        source: '/authors',
-        destination: '/author/authors',
-      },
-      {
-        source: '/publishers',
-        destination: '/publisher/publishers',
-      },
-      {
-        source: '/categories',
-        destination: '/category/categories',
-      },
-      {
-        source: '/category/add',
-        destination: '/category/add',
-      },
-      {
-        source: '/category/:id',
-        destination: '/category/:id',
-      },
-      {
-        source: '/category/:id/edit',
-        destination: '/category/:id/edit',
-      },
-      {
-        source: '/users',
-        destination: '/user/users',
-      }
+      { source: '/signin', destination: '/auth/signin' },
+      { source: '/login', destination: '/auth/login' },
+      { source: '/authors', destination: '/author/authors' },
+      { source: '/publishers', destination: '/publisher/publishers' },
+      { source: '/categories', destination: '/category/categories' },
+      { source: '/category/add', destination: '/category/add' },
+      { source: '/category/:id', destination: '/category/:id' },
+      { source: '/category/:id/edit', destination: '/category/:id/edit' },
+      { source: '/users', destination: '/user/users' },
     ];
   },
-
 };
 
-export default withPlugins([[withTM]], nextConfig);
+export default nextConfig;
