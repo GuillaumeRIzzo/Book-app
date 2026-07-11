f3b46f258bb84b72ae10a1504e21dc011261e54b
GuillaumeRizzo <guillaume.rizzo.lomet@gmail.com>
2026-07-11 16:27:02 +0200

    refactor(book): update book pages

--- Front/ReactJS/src/pages/book/[id]/edit.tsx
+++ Front/ReactJS/src/pages/book/[id]/edit.tsx
@@ -1,7 +1,8 @@
+import { withNoSSR } from "@/components/common/withNoSSR";
 import BookForm from "@/features/books/BookForm";
 
 const EditBook: React.FC = () => {
   return <BookForm />
 }
 
-export default EditBook;
+export default withNoSSR(EditBook);
--- Front/ReactJS/src/pages/book/[id]/index.tsx
+++ Front/ReactJS/src/pages/book/[id]/index.tsx
@@ -1,7 +1,8 @@
+import { withNoSSR } from '@/components/common/withNoSSR';
 import BookDetails from '@/features/books/BookDetails/BookDetails';
 
 const BookDetailsPage: React.FC = () => {
   return <BookDetails />;
 };
 
-export default BookDetailsPage;
+export default withNoSSR(BookDetailsPage);
