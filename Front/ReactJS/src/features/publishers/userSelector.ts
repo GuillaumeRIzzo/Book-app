8033015b91fd018b765cf6c4f0d80bc848120e90
GuillaumeRizzo <guillaume.rizzo.lomet@gmail.com>
2026-07-11 15:41:41 +0200

    refactor(user): reorganize user redux features

--- Front/ReactJS/src/features/publishers/userSelector.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "@/redux/store";
-
-export const selectAllUsers = (state: RootState) => state.users.users;
-export const selectUserStatus = (state: RootState) => state.users.status;
-export const selectUserError = (state: RootState) => state.users.error;
